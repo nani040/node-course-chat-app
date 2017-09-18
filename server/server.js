b0e447004b2ea97af628c6479f0a5a233a140fd8
naveen <nani040@gmail.com>
2017-09-18 10:58:36 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+const publicPath = path.join(__dirname, '../public');
+app.use(express.static(publicPath));
+
+console.log(publicPath);
+
+
+
+app.listen(port, ()=>{
+	console.log(`server starts running on port ${port}`);
+});

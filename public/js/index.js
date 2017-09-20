aa74866ea74a93de72e1b334edbc4a5d13a66ae9
naveen <nani040@gmail.com>
2017-09-20 12:29:22 +0530

    geolocation support

--- public/js/index.js
+++ public/js/index.js
@@ -30,6 +30,16 @@ socket.on('newMessage', function (message){
 	jQuery('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function(message){
+	var li = jQuery('<li></li>');
+	var a = jQuery('<a target="_blank">MY Current Location</a>');
+
+	li.text(`${message.from}: `);
+	a.attr('href', message.url);
+	li.append(a);
+	jQuery('#messages').append(li);
+});
+
 // socket.emit('createMessage', {
 // 	from: 'frank',
 // 	text: 'hello...'
@@ -46,4 +56,23 @@ jQuery('#message-form').on('submit', function(e) {
 	}, function(){
 
 	});
+});
+
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function() {
+	if (!navigator.geolocation){
+		return alert('geolocation not supported by your browser.');
+	}
+
+	navigator.geolocation.getCurrentPosition(function (position) {
+		socket.emit('createLocationMessage', {
+			latitude: position.coords.latitude,
+			longitude: position.coords.longitude
+		});
+
+	}, function(){
+		alert('unable to tetch location.');
+	});
+
 });
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
 var app = express();
@@ -26,6 +26,11 @@ io.on('connection', (socket) => {
     io.emit('newMessage', generateMessage(message.from, message.text));
     callback('This is from server!');
 	});
+
+
+  socket.on('createLocationMessage', (coords) => {
+  	io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+  });
 	// socket.emit('newEmail', {
 	// 	from: 'nani040@gmail.com',
 	// 	text: 'hi hello ted!',
